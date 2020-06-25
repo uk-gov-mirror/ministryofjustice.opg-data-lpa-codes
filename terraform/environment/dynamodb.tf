1f325bbcc7e0b3c77315f28eb4d8a6469e1d93fb
james.warren <james.warren@digital.justice.gov.uk>
2020-06-25 12:34:03 +0100

    IN-274 add workspace deletion

--- terraform/environment/dynamodb.tf
+++ terraform/environment/dynamodb.tf
@@ -38,3 +38,26 @@ resource "aws_dynamodb_table" "lpa_codes" {
 
   tags = local.default_tags
 }
+
+//dynamodb for dev env only for branch deletion
+
+resource "aws_dynamodb_table" "workspace_cleanup_table" {
+  count        = local.account.account_mapping == "development" ? 1 : 0
+  name         = "WorkspaceCleanup"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "WorkspaceName"
+
+  attribute {
+    name = "WorkspaceName"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "ExpiresTTL"
+    enabled        = true
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
